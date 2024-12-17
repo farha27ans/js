220d2dab5c00655990b4f1faea63411264322387
farha27ans <ansarifarha2709@gmail.com>
2024-12-17 15:00:37 +0000

    variables

---
+++ basics/data_types.js
@@ -0,0 +1,30 @@
+"use strict"; // Treat all JS code as newer version
+
+//alert(3+3)// we are using nodejs , not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Zoya")
+
+
+let name = "Zoey"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+//null ek object hai and undefined ka ek special type hai undefined 
+//object
+console.log(typeof age);
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
