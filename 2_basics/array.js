0cd53080eebec66c0c8f265d82e55523c90e77ad
farha27ans <ansarifarha2709@gmail.com>
2025-01-26 15:58:48 +0000

    main

---
+++ 2_basics/array.js
@@ -0,0 +1,23 @@
+// array
+
+const myArr = [0,1,2,3,4,5]
+const myfruit =["apple","banana"]
+
+const myArr2 = new Array(1,2,3,4)
+
+console.log(array[0]);
+
+// Array methods
+
+//myArr.unshift(9)
+//myArr.shift()
+// myArr.push(6)
+// myArr.pop()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+console.log(myArr);
+console.log(newArr);
+console.log(typeof newArr)
